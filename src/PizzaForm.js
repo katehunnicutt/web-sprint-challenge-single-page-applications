421a7e444ff6411e2fe57a393bfcccff6903be66
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-03-26 13:47:16 -0700

    style toppings and instructions

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -19,17 +19,53 @@ const ErrorMessage = styled.p`
   color: darkRed;
   font-size: 0.8rem;
 `
+const Toppings = styled.div`
+  display: grid;
+  grid-template-columns: 14rem 14rem;
+  grid-row-gap: 0.5rem;
 
-// const Inputs =styled.label`
-//   color: green;
-// `
+  & label {
+    display: flex;
+    align-items: center;
+  }
+  & input {
+    margin: 0 0.5rem 0;
+  }
+`
+
+const TextArea = styled.textarea`
+  width: 100%;
+  font-family: Arial, Helvetica, sans-serif;
+  height: 5rem;
+`
 
 export default function PizzaForm() {
+  const [specialInstructions, setSpecialInstructions] = React.useState('')
+  const [
+    toppingsUnderInstructions,
+    setToppingsUnderInstructions
+  ] = React.useState(false)
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(formSchema),
     mode: 'all'
   })
   const onSubmit = data => console.log(data)
+
+  React.useEffect(() => {
+    setToppingsUnderInstructions([
+      'cheese',
+      'pineapple',
+      'spinach',
+      'artichoke hearts',
+      'sundried tomatoes',
+      'goat cheese',
+      'red onion',
+      'green pepper',
+      'red pepper',
+      'banana peppers',
+      'cream cheese'
+    ].some(topping => specialInstructions.includes(topping)))
+  }, [specialInstructions])
 
   return (
     <>
@@ -48,6 +84,82 @@ export default function PizzaForm() {
 
         {errors.size && <ErrorMessage>{errors.size.message}</ErrorMessage>}
 
+        <h2>Choose a Base</h2>
+        <select name="base" ref={register({ required: true })}>
+          <option value="">--Please choose an option--</option>
+          <option value="pomodoro">pomodoro</option>
+          <option value="garlic olive oil">garlic olive oil</option>
+          <option value="basil pesto">basil pesto</option>
+        </select>
+        {errors.base && <ErrorMessage>{errors.base.message}</ErrorMessage>}
+
+        <h2>Add Toppings</h2>
+        <Toppings className="toppings">
+          <label>
+            <input type="checkbox" name="pineapple" ref={register} />
+            pineapple
+          </label>
+
+          <label>
+            <input type="checkbox" name="spinach" ref={register} />
+            spinach
+          </label>
+
+          <label>
+            <input type="checkbox" name="artichokeHearts" ref={register} />
+            artichoke hearts
+          </label>
+
+          <label>
+            <input type="checkbox" name="sundriedTomatoes" ref={register} />
+            sundried tomatoes
+          </label>
+
+          <label>
+            <input type="checkbox" name="goatCheese" ref={register} />
+            goat cheese
+          </label>
+
+          <label>
+            <input type="checkbox" name="redOnion" ref={register} />
+            red onion
+          </label>
+
+          <label>
+            <input type="checkbox" name="greenPeppers" ref={register} />
+            green pepper
+          </label>
+
+          <label>
+            <input type="checkbox" name="redPeppers" ref={register} />
+            red pepper
+          </label>
+
+          <label>
+            <input type="checkbox" name="bananaPeppers" ref={register} />
+            banana peppers
+          </label>
+
+          <label>
+            <input type="checkbox" name="creamCheese" ref={register} />
+            cream cheese
+          </label>
+        </Toppings>
+
+        <h2> Special Instructions</h2>
+        <TextArea
+          value={specialInstructions}
+          onChange={e => setSpecialInstructions(e.target.value)}
+          name="specialInstructions"
+          ref={register}
+        />
+        {toppingsUnderInstructions && (
+          <p style={{ margin: '0' }}>
+            * do not add toppings here, some special intructions could result in
+            an increase of price after checkout
+          </p>
+        )}
+
         <label>
           email
           <input type="email" name="email" ref={register} />
@@ -60,87 +172,6 @@ export default function PizzaForm() {
         </label>
         {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
         {errors.name && <ErrorMessage>{errors.name.min}</ErrorMessage>}
-
-        <h2>Choose a Base</h2>
-        <select name="base" ref={register({ required: true })}>
-          <option value="">--Please choose an option--</option>
-          <option value="pomodoro">pomodoro</option>
-          <option value="garlic olive oil">garlic olive oil</option>
-          <option value="basil pesto">basil pesto</option>
-        </select>
-        {errors.base && <ErrorMessage>{errors.base.message}</ErrorMessage>}
-
-        <h2>Add Toppings</h2>
-        <div className="toppings">
-          <label>
-            {' '}
-            pineapple
-            <input type="checkbox" name="pineapple" ref={register} />
-          </label>
-
-          <label>
-            {' '}
-            spinach
-            <input type="checkbox" name="spinach" ref={register} />
-          </label>
-
-          <label>
-            artichoke hearts
-            <input type="checkbox" name="artichokeHearts" ref={register} />
-          </label>
-
-          <label>
-            {' '}
-            sundried tomatoes
-            <input type="checkbox" name="sundriedTomatoes" ref={register} />
-          </label>
-
-          <label>
-            {' '}
-            goat cheese
-            <input type="checkbox" name="goatCheese" ref={register} />
-          </label>
-
-          <label>
-            {' '}
-            red onion
-            <input type="checkbox" name="redOnion" ref={register} />
-          </label>
-
-          <label>
-            {' '}
-            green pepper
-            <input type="checkbox" name="greenPeppers" ref={register} />
-          </label>
-
-          <label>
-            {' '}
-            red pepper
-            <input type="checkbox" name="redPeppers" ref={register} />
-          </label>
-
-          <label>
-            {' '}
-            banana peppers
-            <input type="checkbox" name="bananaPeppers" ref={register} />
-          </label>
-
-          <label>
-            {' '}
-            cream cheese
-            <input type="checkbox" name="creamCheese" ref={register} />
-          </label>
-        </div>
-
-        <label htmlFor="specialInstructions">
-          Special Instructions
-          <input type="text" name="specialInstructions" ref={register} />
-          <p>
-            {' '}
-            * do not add toppings here, some special intructions could result in
-            an increase of price after checkout
-          </p>
-        </label>
 
         <input type="submit" />
         <button type="submit">Order Now</button>
